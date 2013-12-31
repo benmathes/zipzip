1c7f4138420df7aec05dcd110a20e6e52b0e1f35
Ben Mathes <mathes.ben@gmail.com>
2013-12-31 12:52:28 -0800

    use clojure 1.5.1

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://zipzip.herokuapp.com"
   :license {:name "FIXME: choose"
             :url "http://example.com/FIXME"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.1"]
                  [ring/ring-jetty-adapter "1.1.0"]
                  [ring/ring-devel "1.1.0"]
